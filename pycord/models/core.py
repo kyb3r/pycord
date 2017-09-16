fd5a861c014cbedf748ca74ce7abb1c2a48d25fb
verixx <abdurraqeeb53@gmail.com>
2017-09-17 01:25:06 +1000

    Added ABC's for SnowFlake and User

--- pycord/models/core.py
+++ pycord/models/core.py
@@ -1,8 +1,67 @@
+from abc import ABC, abstractmethod
 
 
-class Sendable:
+
+class Snowflake(ABC):
+    '''Everything is probably inherited from this'''
+    __slots__ - ()
+
+    attributes = ('id', 'created_at')
+
+    @abstractmethod
+    def created_at(self):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, klass):
+        if cls is Snowflake:
+            for attr in cls.attributes:
+                for base in klass.__mro__:
+                    if attr in base.__dict__:
+                        break
+                else:
+                    return NotImplemented
+            return True
+        else:
+            return NotImplemented
+
+
+class User(ABC):
+
+    __slots__ = ()
+
+    attributes = (
+        'nickname', 'name', 'mention', 
+        'avatar', 'discrim', 'is_bot'
+        )
+
+    @property
+    @abstractmethod
+    def mention(self):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, klass):
+        if cls is User:
+            if Snowflake.__subclasshook__(klass) is NotImplemented:
+                return NotImplemented
+            for attr in cls.attributes:
+                for base in klass.__mro__:
+                    if attr in base.__dict__:
+                        break
+                else:
+                    return NotImplemented
+            return True
+        return NotImplemented
+
+
+
+
+class Sendable(ABC):
     '''Base class that TextChannels, Users and PrivateChannels inherit from'''
 
+    __slots__ = ()
+    
     def send(self, content=None, embed=None, file=None, tts=False):
         pass
 
