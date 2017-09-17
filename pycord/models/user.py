69ba4ea0c8a9f065156c6b480de82de06575aa01
verixx <abdurraqeeb53@gmail.com>
2017-09-17 17:33:21 +1000

    Added a clientuser and a Member class

--- pycord/models/user.py
+++ pycord/models/user.py
@@ -1,16 +1,19 @@
-from .core import abstractmethod
 from .core import Snowflake, Sendable, Serializable
 
+
+
 class User(Snowflake, Sendable, Serializable):
+
     __slots__ = (
-        'nickname', 'name', 'avatar',
-        'discrim', 'is_bot', 'verified')
+        'username', 'avatar','id',
+        'discriminator', 'bot', 'verified'
+        )
 
     def __init__(self, data={}):
         self.from_dict(data)
 
     def from_dict(self, data):
-        for key in __class__.__slots__:
+        for key in self.__slots__:
             if key in data and data[key] is not None:
                 setattr(self, key, data[key])
 
@@ -19,3 +22,50 @@ class User(Snowflake, Sendable, Serializable):
 
     async def trigger_typing(self):
         pass
+
+
+class ClientUser(User):
+
+    __slots__ = (
+        'email', 'mfa_enabled','username', 'avatar',
+        'discriminator', 'bot', 'verified','id'
+        )
+
+    def __init__(self, data={}, state=None):
+        super().__init__()
+        self._state = state
+
+        for key in self.__slots__:
+            if key in data:
+                setattr(self, key, data[key])
+
+
+
+class Member(User):
+
+    __slots__ = (
+        '_roles', 'username', 'avatar','id',
+        'discriminator', 'bot', 'verified',
+        'guild', 'nick'
+        )
+
+    def __init__(self, data, guild, state):
+        super().__init__()
+        self._state = state
+        self.guild = guild
+        self.id = int(data['user'].get('id'))
+        self.bot = data.get('bot')
+        self.verified = data.get('verified')
+        self.from_data(data)
+
+    def from_data(self, data):
+        self.username = data.get('username')
+        self.avatar = data.get('avatar')
+        self.discriminator = data.get('discriminator')
+        if data.get('nick'):
+            self.nick = data.get('nick')
+
+
+# not done yet
+
+
