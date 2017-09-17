45b6777d2eaf82bcf289d513b2899b88c2200c02
verixx <abdurraqeeb53@gmail.com>
2017-09-17 17:30:49 +1000

    Made an event handler class
    Instead of just if elses

--- pycord/api/__init__.py
+++ pycord/api/__init__.py
@@ -1,2 +1,3 @@
 from .http import HttpClient
 from .gateway import ShardConnetion
+from .events import EventHandler
--- pycord/api/events.py
+++ pycord/api/events.py
@@ -1,139 +1,55 @@
 from ..utils import json
+from ..models import ClientUser, Guild
+from collections import deque
 
-async def Handle(self, event, data):
+class EventHandler:
+    def __init__(self, client):
+        self.client = client
+        self.loop = client.loop
+        self.http = client.api
 
-    print("Handling event:", event, json.dumps(data, sort_keys=True, indent=2))
+    def reset(self):
+        self.user = None
+        self.guilds = {}
+        self.users = {}
+        self.emojis = {}
+        self.messages = deque(maxlen=2000)
 
-    if event == 'READY':
-        await self.client.emit('ready') # not supposed to be here
-        self.client.user.from_dict(data.get('user'))
-        self.session_id = data.get('session_id')
-        self.resume = False
+    def add_emoji(self, emoji):
+        self.emojis[emoji.id] = emoji
 
-    elif event == 'RESUMED':
-        await self.client.emit('resume')
-        self.resume = False
+    def get_guild(self, id):
+        return self.guilds.get(id)
 
-    elif event == 'CHANNEL_CREATE':
-        await self.client.emit('channel_create')
-        pass
+    def add_guild_from_data(self, data):
+        guild = Guild(data, self)
+        self.guilds[guild.id] = guild
 
-    elif event == 'CHANNEL_UPDATE':
-        await self.client.emit('channel_update')
-        pass
+    def add_guild(self, guild):
+        self.guilds[guild.id] = guild
 
-    elif event == 'CHANNEL_DELETE':
-        await self.client.emit('channel_delete')
-        pass
+    async def handle_ready(self, data):
+        self.reset()
 
-    elif event == 'CHANNEL_PINS_UPDATE':
-        await self.client.emit('pins_update')
-        pass
+        self.user = ClientUser(data['user'], self)
+        for guild in data['guilds']:
+            self.add_guild_from_data(guild)
 
-    elif event == 'GUILD_CREATE':
+        await self.client.emit('ready')
+
+        # have to somehow block the self.client.emit until all the guilds are parsed
+
+    async def handle_message_create(self, data):
+        msg = Message(data, self)
+        await self.client.emit('message', msg)
+
+    async def handle_guild_create(self, data):
+        print('created guilds')
+        guild = self.guilds.get(int(data['id']))
+        if guild:
+            guild.from_data(data)
+            self.add_guild(guild)
+        else:
+            self.add_guild_from_data(data)
+
         await self.client.emit('guild_create')
-        pass
-
-    elif event == 'GUILD_UPDATE':
-        await self.client.emit('guild_update')
-        pass
-
-    elif event == 'GUILD_DELETE':
-        await self.client.emit('guild_delete')
-        pass
-
-    elif event == 'GUILD_BAN_ADD':
-        await self.client.emit('member_ban')
-        pass
-
-    elif event == 'GUILD_BAN_REMOVE':
-        await self.client.emit('member_unban')
-        pass
-
-    elif event == 'GUILD_EMOJIS_UPDATE':
-        await self.client.emit('emoji_update')
-        pass
-
-    elif event == 'GUILD_INTERGRATIONS_UPDATE':
-        await self.client.emit('intergrations_update')
-        pass
-
-    elif event == 'GUILD_MEMBER_ADD':
-        await self.client.emit('member_join')
-        pass
-
-    elif event == 'GUILD_MEMBER_REMOVE':
-        await self.client.emit('member_leave')
-        pass
-        
-    elif event == 'GUILD_MEMBERS_CHUNK':
-        await self.client.emit('guild_members_chunk')
-        pass
-
-    elif event == 'GUILD_ROLE_CREATE':
-        await self.client.emit('role_create')
-        pass
-
-    elif event == 'GUILD_ROLE_UPDATE':
-        await self.client.emit('role_update')
-        pass
-
-    elif event == 'GUILD_ROLE_DELETE':
-        await self.client.emit('role_delete')
-        pass
-
-    elif event == 'MESSAGE_CREATE':
-        await self.client.emit('message', data) # for testing
-        pass
-
-    elif event == 'MESSAGE_UPDATE':
-        await self.client.emit('message_edit')
-        pass
-
-    elif event == 'MESSAGE_DELETE':
-        await self.client.emit('message_delete')
-        pass
-
-    elif event == 'MESSAGE_DELETE_BULK':
-        await self.client.emit('message_delete_bulk')
-        pass
-
-    elif event == 'MESSAGE_REACTION_ADD':
-        await self.client.emit('reaction_add')
-        pass
-
-    elif event == 'MESSAGE_REACTION_REMOVE':
-        await self.client.emit('reaction_remove')
-        pass
-
-    elif event == 'MESSAGE_REACTION_REMOVE_ALL':
-        await self.client.emit('reaction_remove_all')
-        pass
-
-    elif event == 'PRESENCE_UPDATE':
-        await self.client.emit('presence_update')
-        pass
-
-    elif event == 'GAME_OBJECT':
-        await self.client.emit('game_object')
-        pass
-
-    elif event == 'TYPING_START':
-        await self.client.emit('typing_start')
-        pass
-
-    elif event == 'USER_UPDATE':
-        await self.client.emit('user_update')
-        pass
-
-    elif event == 'VOICE_STATE_UPDATE':
-        await self.client.emit('voice_state_update')
-        pass
-
-    elif event == 'VOICE_SERVER_UPDATE':
-        await self.client.emit('voice_server_update')
-        pass
-
-    elif event == 'WEBHOOKS_UPDATE':
-        await self.client.emit('webhook_update')
-        pass
