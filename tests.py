54de93d35ab7f5d3c93fbdd4e8afdab5a423f8ab
verixx <abdurraqeeb53@gmail.com>
2017-09-19 01:04:41 +1000

    did a lot of testing and fixed some fatal errors

--- tests.py
+++ tests.py
@@ -1,17 +1,31 @@
 import pycord
+import traceback
+import time
 
 client = pycord.Client()
 
 @client.on('ready')
 async def on_ready():
+    bootup = time.time() - client._boot_up_time
+    print('--------------------')
+    print(f'Bootup time: {bootup:.2f} secs')
     print(f'Logged in as: {client.user}')
-    print(f'User ID: {client.user.id}')
-    print(f'Is Bot: {client.user.bot}')
     print(f'With {len(client.guilds)} guilds')
+    print(f'And {len(client.users)} users') # gotta do some chunking
+    print(f'User ID: {client.user.id}')
+    print(f'Is Bot: {client.is_bot}')
+    print('--------------------')
 
 @client.on('message')
 async def on_message(message):
     if message.content.startswith('py.ping'):
         await message.reply('Pong!')
+    if message.content.startswith('py.guildinfo'):
+        await message.reply(str(message.guild))
 
-client.login('token')
+@client.on('error')
+async def error_handler(e):
+    traceback.print_exc()
+    
+
+client.login("token")
