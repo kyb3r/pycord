9b5129f1c7a6709623a5e81eedc09a49d840defd
verixx <abdurraqeeb53@gmail.com>
2017-09-18 20:20:38 +1000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,42 +1,17 @@
-from pycord import Client
-import asyncio
+import pycord
 
-client = Client()
-
-class Channel: #placeholder
-    def __init__(self, id):
-        self.id = id
+client = pycord.Client()
 
 @client.on('ready')
-async def ready_event():
-    print(f'Logged in as: {client.user.username}')
+async def on_ready():
+    print(f'Logged in as: {client.user}')
     print(f'User ID: {client.user.id}')
     print(f'Is Bot: {client.user.bot}')
-
-    for guild in client.guilds:
-        print(guild.id, guild.unavailable)
+    print(f'With {len(client.guilds)} guilds')
 
 @client.on('message')
-async def command_test(data):
-    channel = Channel(int(data['channel_id']))
-    if data['content'].startswith('py.ping'):
-        await client.api.send_message(channel, content='pong!')
+async def on_message(message):
+    if message.content.startswith('py.ping'):
+        await message.reply('Pong!')
 
 client.login('token')
-
-# from pycord.models.user import ClientUser
-
-# user = {
-#     "avatar":"fb405db5b7847c5f113ece6e33e0d51f",
-#     "bot":True,
-#     "discriminator":"5599",
-#     "email": None,
-#     "id":"354218232426528768",
-#     "mfa_enabled": True,
-#     "username":"API-Tests",
-#     "verified": True
-# }
-
-# test = ClientUser(user)
-
-# print(test.username)
