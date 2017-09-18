c8d918fd91d5edb6ec93c41b7824f05207267d95
verixx <abdurraqeeb53@gmail.com>
2017-09-19 00:58:56 +1000

    Added support for selfbots
    Although we have to deal with chunking guilds, we havent done that yet...

--- pycord/api/events.py
+++ pycord/api/events.py
@@ -1,7 +1,31 @@
-from ..utils import json
+'''
+MIT License
+
+Copyright (c) 2017 verixx / king1600
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+from pycord.models import ClientUser, Guild, Message
+from pycord.utils import json
 from collections import deque
-from ..models import Message
-from ..models import ClientUser, Guild, Message
+
 
 class EventHandler:
     def __init__(self, shard):
@@ -15,6 +39,10 @@ class EventHandler:
         for guild in data['guilds']:
             self.client.guilds.add(Guild(self.client, guild))
 
+        if not self.client.is_bot and not self.emitted_ready:
+            await self.client.emit('ready')
+            self.emitted_ready = True
+
     async def handle_message_create(self, data):
         message = Message(self.client, data)
         self.client.messages.add(message)
@@ -27,6 +55,7 @@ class EventHandler:
         else:
             self.client.guilds.add(Guild(self.client, data))
 
-        if len([None for g in self.client.guilds if g.unavailable]) == 0:
-            await self.client.emit('ready')
+        if not self.emitted_ready:
+            if len([None for g in self.client.guilds if g.unavailable]) == 0:
+                await self.client.emit('ready')
 
