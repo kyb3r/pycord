f5d02237be395b2a66871b3bd03ee970f5440215
verixx <abdurraqeeb53@gmail.com>
2017-09-19 01:03:21 +1000

    Added error handling for events

--- pycord/utils/emitter.py
+++ pycord/utils/emitter.py
@@ -1,3 +1,27 @@
+'''
+MIT License
+
+Copyright (c) 2017 verixx / king1600
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 import asyncio
 from abc import ABC
 
@@ -22,12 +46,13 @@ class Emitter(ABC):
 
     async def emit(self, event, *args, **kwargs):
         on_event = f'on_{event}'
-        if hasattr(self, on_event):
-            await getattr(self, on_event)(*args, **kwargs)
-        if event in self._events:
-            if hasattr(self, event):
-                await getattr(self, event)
-            for callback in self._events[event]:
-                await callback(*args, **kwargs)
+        try:
+            if hasattr(self, on_event):
+                await getattr(self, on_event)(*args, **kwargs)
+            if event in self._events:
+                for callback in self._events[event]:
+                    await callback(*args, **kwargs)
+        except Exception as e:
+            await self.emit('error', e)
 
 
