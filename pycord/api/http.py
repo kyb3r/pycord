3dcf7e3ccb87ef82c7107674e3a6574b3c813226
verixx <abdurraqeeb53@gmail.com>
2017-09-19 00:59:28 +1000

    Made the imports easier to followe

--- pycord/api/http.py
+++ pycord/api/http.py
@@ -1,11 +1,36 @@
-import sys
-import time
-import asyncio
-import aiohttp
-import weakref
+'''
+MIT License
+
+Copyright (c) 2017 verixx / king1600
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+from pycord.utils import get_event_loop, to_json, from_json, API
+from pycord import __version__, __github__
 from urllib.parse import quote
-from .. import __version__, __github__
-from ..utils import get_event_loop, to_json, from_json, API
+import weakref
+import aiohttp
+import asyncio
+import time
+import sys
+
 
 class HoldableLock:
     __slots__ = ('lock', 'unlock')
