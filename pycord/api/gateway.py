029a4220731ff572ae648616ae8a2f284b356285
verixx <abdurraqeeb53@gmail.com>
2017-09-19 00:59:17 +1000

    Made the imports easier to followe

--- pycord/api/gateway.py
+++ pycord/api/gateway.py
@@ -1,36 +1,42 @@
-import time
-import zlib
-import asyncio
-import platform
-import traceback
+'''
+MIT License
+
+Copyright (c) 2017 verixx / king1600
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+from pycord.api.events import EventHandler
+from pycord.utils import get_libname, API
+from pycord.utils import to_json, from_json
+from pycord.utils import json
 import websockets
-import threading
-from ..utils import json
-from .events import EventHandler
-from ..utils import get_libname, API
-from ..utils import to_json, from_json
-
-
-
-# class HeartbeatTask(threading.Thread):
-#     def __init__(self, connection, interval):
-#         super().__init__()
-#         self.conn = connection
-#         self.interval = interval
-#         self.stop_event = threading.Event()
-
-#     def run(self):
-#         while not self.stop_event.wait(self.interval):
-#             if self.conn.heartbeat_acked:
-#                 coro = self.conn.send(self.conn.HEARTBEAT, self.conn.sequence)
-#                 asyncio.run_coroutine_threadsafe(coro, loop=self.conn.loop)
-#                 coro.result()
-
-#     def stop(self):
-#         self.stop_event.close()
+import traceback
+import platform
+import asyncio
+import zlib
+import time
 
 
 class ShardConnection:
+
+    '''Represents a websocket connection to a shard.'''
 
     DISPATCH        = 0
     HEARTBEAT       = 1
