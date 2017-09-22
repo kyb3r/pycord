ccef162e7f663f91ddc76e2c8f1fb7c2b1dddba4
verixx <abdurraqeeb53@gmail.com>
2017-09-22 13:49:08 +1000

    updated

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'py-cord',
   packages = ['pycord'], # this must be the same as the name above
-  version = 'v0.2.6-alpha',
+  version = 'v0.2.7-alpha',
   description = 'An async discord api wrapper',
   author = 'verixx',
   author_email = 'abdurraqeeb53@gmail.com',
   url = 'https://github.com/verixx/pycord', # use the URL to the github repo
-  download_url = 'https://github.com/verixx/pycord/archive/v0.2.6-alpha.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/verixx/pycord/archive/v0.2.7-alpha.tar.gz', # I'll explain this in a second
   keywords = ['discord'], # arbitrary keywords
   classifiers = [],
 )
