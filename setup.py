898613d3a83de08f743d2c9f0497ec86397624db
Jori van Ee <jorivanee@hotmail.com>
2017-09-28 15:46:51 +0200

    Update setup.py
    Update authors and bump version

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ if sys.argv[-1] == 'publish':
 setup(
     name='pycord',
     packages=['pycord', 'pycord.utils', 'pycord.models', 'pycord.api'],  # this must be the same as the name above
-    version='v0.3.4-alpha',
+    version='v0.3.5-alpha',
     description='A Discord API library for Python running on the Trio and Curio async libraries',
-    author='verixx',
+    author='JustMaffie, verixx, king1600, henry232323',
     author_email='abdurraqeeb53@gmail.com',
     url='https://github.com/verixx/pycord',  # use the URL to the github repo
     download_url='https://github.com/verixx/pycord/archive/v0.3.4-alpha.tar.gz',  # I'll explain this in a second
