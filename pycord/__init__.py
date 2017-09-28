f5a3ac0cd486d3a25ef4e135a2f73977638a5868
Jori van Ee <jorivanee@hotmail.com>
2017-09-28 16:10:36 +0200

    Update __init__.py

--- pycord/__init__.py
+++ pycord/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "pycord"
 __license__ = "MIT"
 __version__ = "0.2.4a"
-__author__ = ["verixx", "king1600", "henry232323"]
+__author__ = ["JustMaffie", "verixx", "king1600", "henry232323"]
 __github__ = 'https://github.com/verixx/pycord'
 
 from .client import Client
