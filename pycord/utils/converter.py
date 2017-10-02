45feb518cf09f7c3384bf8178cff22bec0f0e3fd
verixx <abdurraqeeb53@gmail.com>
2017-10-02 15:16:43 +1100

    Started on converters

--- pycord/utils/converter.py
+++ pycord/utils/converter.py
@@ -3,9 +3,14 @@ from abc import ABC, abstractmethod
 class Converter(ABC):
 
     @abstractmethod
-    def __init__(self, msg, value):
+    def __init__(self, ctx, value):
         pass
 
     @abstractmethod
     def convert(self):
         pass
+
+
+def member(ctx, argument):
+    '''Converts arguments to a member object.'''
+    client = ctx.client
