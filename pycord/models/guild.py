b0e30fd5d2533ff2b76300b57117666c2c9634a2
verixx <abdurraqeeb53@gmail.com>
2017-11-24 21:27:18 +1100

    Added a game class and supported presences

--- pycord/models/guild.py
+++ pycord/models/guild.py
@@ -30,10 +30,24 @@ from ..models.role import Role
 from ..models.user import Member, User
 from ..utils import Collection
 
+class Game:
+    def __init__(self, game):
+        self.name = game.get('name')
+        self.type = game.get('type')
+        # TODO: Enum for type
+    
+    def __str__(self):
+        types = {
+            0: 'Playing',
+            1: 'Streaming',
+            2: 'Listening to',
+            3: 'Watching',
+        }
+        return f'{types.get(self.type)} {self.name}'
 
 class Guild(Snowflake, Serializable):
     __slots__ = (
-        '_members', '_channels', '_emojis', '_roles',
+        'members', 'channels', 'emojis', 'roles',
         'afk_timeout', 'afk_channel', 'icon',
         'name', 'unavailable', 'name', 'region',
         'default_role', 'member_count', 'large',
@@ -45,10 +59,10 @@ class Guild(Snowflake, Serializable):
         if data is None:
             data = {}
         self.client = client
-        self._roles = Collection(Role)
-        self._emojis = Collection(Emoji)
-        self._members = Collection(Member)
-        self._channels = Collection(Channel)
+        self.roles = Collection(Role)
+        self.emojis = Collection(Emoji)
+        self.members = Collection(Member)
+        self.channels = Collection(Channel)
         self.id = int(data.get("id"), 0)
         self.name = None
         self.icon = None
@@ -68,15 +82,16 @@ class Guild(Snowflake, Serializable):
 
     def from_dict(self, data):
         self.id = int(data.get('id', 0))
-        self.name = data['name']
-        self.icon = data['icon']
-        self.region = data['region']
+        self.name = data.get('name')
+        self.icon = data.get('icon')
+        self.region = data.get('region')
         self.splash = data.get('splash')
-        self.mfa_level = data['mfa_level']
+        self.mfa_level = data.get('mfa_level')
         self.features = data.get('features', [])
         self.unavailable = data.get('unavailable', True)
         self.verification_level = data.get('verification_level')
         self.explicit_content_filter = data.get('explicit_content_filter', False)
+        self.owner_id = int(data.get('owner_id'))
 
         for channel_data in data.get('channels', []):
             chan_type = channel_data.get('type', 0)
@@ -87,13 +102,13 @@ class Guild(Snowflake, Serializable):
             else:
                 channel = CategoryChannel(self, channel_data)
             self.client.channels.add(channel)
-            self._channels.add(channel)
+            self.channels.add(channel)
 
         for role in data.get('roles', []):
-            self._roles.add(Role(self, role))
+            self.roles.add(Role(self, role))
 
         for emoji in data.get('emojis', []):
-            self._emojis.add(Emoji(self, emoji))
+            self.emojis.add(Emoji(self, emoji))
 
         for member in data.get('members', []):
             user = member.get('user')
@@ -105,15 +120,36 @@ class Guild(Snowflake, Serializable):
                 else:
                     user = self.client.users.get(user_id)
 
-            self._members.add(Member(self.client, self, user, member))
+            self.members.add(Member(self.client, self, user, member))
+        
+        for presence in data.get('presences', []):
+            member = self.members.get(int(presence['user']['id']))
+            if member is None:
+                continue
+            game = presence.get('game')
+            member.game = Game(game) if game else None
+            member.status = presence.get('status')
+            # print(member.status)
+            if not member.bot:
+                member.user.status = member.status
+                member.user.game = member.game
+
+        self.owner = self.members.get(self.owner_id)
+    
+    @property
+    def icon_url(self):
+        return self.icon_url_as()
+    
+    def icon_url_as(self, format='png', size=1024):
+        return 'https://cdn.discordapp.com/icons/{0.id}/{0.icon}.{1}?size={2}'.format(self, format, size)
 
     @property
     def text_channels(self):
-        return tuple(filter(lambda x: isinstance(x, TextChannel), self._channels))
+        return tuple(filter(lambda x: isinstance(x, TextChannel), self.channels))
 
     @property
     def voice_channels(self):
-        return tuple(filter(lambda x: isinstance(x, VoiceChannel), self._channels))
+        return tuple(filter(lambda x: isinstance(x, VoiceChannel), self.channels))
 
     def add_member(self, member):
-        self._members[member.id] = member
+        self.members[member.id] = member
